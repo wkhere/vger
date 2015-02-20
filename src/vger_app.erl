53f5bfcc95ea3e3b571e2eda0b7041cea9786214
Wojciech Kaczmarek <wojciech.kaczmarek@pearson.com>
2015-02-20 10:55:15 +0100

    rebarize

---
+++ src/vger_app.erl
@@ -0,0 +1,16 @@
+-module(vger_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    vger_sup:start_link().
+
+stop(_State) ->
+    ok.
