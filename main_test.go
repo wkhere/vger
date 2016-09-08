3cb30b73e068617a93accf5e7d2a1c2adbba8b9e
Wojciech Kaczmarek <wojtekk@kofeina.net>
2016-09-08 12:58:51 +0200

    go ver: cleanup comments

--- main_test.go
+++ main_test.go
@@ -88,8 +88,6 @@ func BenchmarkNbs(b *testing.B) {
 }
 
 func ExampleAstar() {
-	// The result here is different than for Py. By reordering the nbs I got
-	// rid of some 'slalom'.
 	fmt.Print(astarWellKnownPath())
 	// Output:
 	// [{enioar 2 2} {enioar 3 3} {enioar 4 4} {enioar 5 4} {enioar 6 4} {enioar 7 4} {enioar 8 4} {enioar 9 4} {enioar 10 4} {enioar 11 4} {enioar 12 4} {enioar 13 4} {enioar 14 5} {enioar 15 6} {enioar 16 7} {enioar 17 7} {enioar 18 7} {enioar 19 7} {enioar 20 7}]
