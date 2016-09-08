f7c02ad6a6fa1b8417904830e903585ac69136ae
Wojciech Kaczmarek <wojtekk@kofeina.net>
2016-09-08 13:08:43 +0200

    go ver: more tests incl. quickcheck

--- main_test.go
+++ main_test.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"reflect"
 	"testing"
+	"testing/quick"
 )
 
 func TestMain(m *testing.M) {
@@ -93,8 +96,85 @@ func ExampleAstar() {
 	// [{enioar 2 2} {enioar 3 3} {enioar 4 4} {enioar 5 4} {enioar 6 4} {enioar 7 4} {enioar 8 4} {enioar 9 4} {enioar 10 4} {enioar 11 4} {enioar 12 4} {enioar 13 4} {enioar 14 5} {enioar 15 6} {enioar 16 7} {enioar 17 7} {enioar 18 7} {enioar 19 7} {enioar 20 7}]
 }
 
+func ExampleMoreAstar() {
+	env := Env{Ion}
+	fmt.Println(Astar(env, Coord{"enioar", 4, 12}, Coord{"enioar", 5, 3}))
+	// Output:
+	// [{enioar 4 11} {enioar 5 10} {enioar 5 9} {enioar 5 8} {enioar 6 7} {enioar 7 6} {enioar 7 5} {enioar 6 4} {enioar 5 3}]
+}
+
+// generator for quick.Check:
+func (c Coord) Generate(rand *rand.Rand, size int) reflect.Value {
+	c.Sector = "enioar"
+	bbox := envbbox[c.Sector]
+	c.X = rand.Intn(bbox.w + 1)
+	c.Y = rand.Intn(bbox.h + 1)
+	return reflect.ValueOf(c)
+}
+
+func TestPathsAreStraight(t *testing.T) {
+	env := Env{Ion}
+	f := func(p0 Coord) bool {
+		if envdata[p0] == Space {
+			defer func() {
+				recover() // this relies on default bool = false
+			}()
+			checkY(p0, Astar(env, p0, findStraightX(p0, +1)))
+			checkY(p0, Astar(env, p0, findStraightX(p0, -1)))
+			checkX(p0, Astar(env, p0, findStraightY(p0, +1)))
+			checkX(p0, Astar(env, p0, findStraightY(p0, -1)))
+		}
+		return true
+	}
+	if err := quick.Check(f, nil); err != nil {
+		t.Error(err)
+	}
+}
+
 func BenchmarkAstar(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		astarWellKnownPath()
+	}
+}
+
+// private
+
+func findStraightX(p0 Coord, dx int) Coord {
+	p := p0
+	for xi := p0.X + 1; ; xi += dx {
+		pi := Coord{p0.Sector, xi, p0.Y}
+		if envdata[pi] != Space {
+			break
+		}
+		p = pi
+	}
+	return p
+}
+
+func findStraightY(p0 Coord, dy int) Coord {
+	p := p0
+	for yi := p0.Y + 1; ; yi += dy {
+		pi := Coord{p0.Sector, p0.X, yi}
+		if envdata[pi] != Space {
+			break
+		}
+		p = pi
+	}
+	return p
+}
+
+func checkY(p0 Coord, path []Node) {
+	for _, p := range path {
+		if p.(Coord).Y != p0.Y {
+			panic("Y differs")
+		}
+	}
+}
+
+func checkX(p0 Coord, path []Node) {
+	for _, p := range path {
+		if p.(Coord).X != p0.X {
+			panic("X differs")
+		}
 	}
 }
